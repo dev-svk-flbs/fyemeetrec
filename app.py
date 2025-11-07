7fdec93ba5c17f3d15d4100eaa98c52629893924
dev-svk-flbs <souvik@fyelabs.com>
2025-11-07 13:39:25 -0500

    before first attempt at pacaging

--- app.py
+++ app.py
@@ -3617,4 +3617,4 @@ if __name__ == '__main__':
     # Start the retry scheduler after Flask is ready
     scheduler = start_scheduler()
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
